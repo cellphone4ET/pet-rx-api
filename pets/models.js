984f9533c9229ef37cf79ab0275c2591607a144c
Erica Johnson <ericacjohnson@gmail.com>
2018-10-04 06:50:02 -0700

    auth related files uncommented

--- pets/models.js
+++ pets/models.js
@@ -23,7 +23,7 @@ const petSchema = mongoose.Schema({
   checkups: [{ type: String }],
   vaccinations: [{ type: String }],
   weight_history: [{ type: String }]
-  // user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" }
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" }
 });
 
 petSchema.methods.serialize = function() {
