4ca49a3f9edbcc7bf9d4406778575f9edcbc102f
Erica Johnson <ericacjohnson@gmail.com>
2018-10-03 19:30:58 -0700

    test port

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:8080";
 exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/pet-rx";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL || "mongodb://localhost/pet-rx-test";
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT;
 
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || "7d";
